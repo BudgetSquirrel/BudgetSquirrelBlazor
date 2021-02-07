55c154d629a04661d394c433849b10be3804372c
Ian Kirkpatrick <ikirkpat@oaisd.org>
2021-02-07 11:48:29 -0500

    Add fluent migrator

---
+++ database/BudgetSquirrel.Dal.Schema/Initial.cs
@@ -0,0 +1,28 @@
+using System.Data;
+using FluentMigrator;
+
+namespace BudgetSquirrel.Dal.Schema.Auth
+{
+  [Migration(202102070833, "Initial Users Schema")]
+  public class Initial : Migration
+  {
+    public override void Up()
+    {
+      Create.Table("Users")
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("FirstName").AsString(45)
+            .WithColumn("LastName").AsString(45);
+      Create.Table("Accounts")
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("Email").AsString(75).NotNullable()
+            .WithColumn("Password").AsString(255).NotNullable()
+            .WithColumn("UserId").AsInt64().NotNullable().ForeignKey("Users", "Id").OnDelete(Rule.Cascade);
+    }
+
+    public override void Down()
+    {
+      Delete.Table("Accounts");
+      Delete.Table("Users");
+    }
+  }
+}
---
+++ database/BudgetSquirrel.Dal.Schema/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using BudgetSquirrel.Dal.Schema.Auth;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BudgetSquirrel.Dal.Schema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = GetConfigurations(args);
+            var serviceProvider = CreateServices(config);
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+        
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </summary>
+        private static IServiceProvider CreateServices(IConfiguration config)
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSqlServer()
+                    // Set the connection string
+                    .WithGlobalConnectionString(config.GetConnectionString("DefaultConnection"))
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(Initial).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+
+        private static IConfiguration GetConfigurations(string[] args)
+        {
+            string envName = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+            return config;
+        }
+    }
+}
