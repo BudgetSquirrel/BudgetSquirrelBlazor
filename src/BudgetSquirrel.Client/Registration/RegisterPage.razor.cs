a6fea15808f34aab741d4f106dee140c57c43e86
Ian Kirkpatrick <ikirkpat@oaisd.org>
2021-02-16 17:48:05 -0500

    Add form state management for register page

--- src/BudgetSquirrel.Client/Registration/RegisterPage.razor.cs
+++ src/BudgetSquirrel.Client/Registration/RegisterPage.razor.cs
@@ -1,4 +1,5 @@
 using System;
+using BudgetSquirrel.Client.Common;
 using Microsoft.AspNetCore.Components;
 
 namespace BudgetSquirrel.Client.Registration
@@ -13,7 +14,39 @@ namespace BudgetSquirrel.Client.Registration
       public string ConfirmPassword { get; set; }
     }
 
-    public Form FormValues = new Form();
+    public class FormValidState
+    {
+      public ValidState IsFullNameValid { get; set; } = ValidState.Empty;
+      public ValidState IsEmailValid { get; set; } = ValidState.Empty;
+      public ValidState IsPasswordValid { get; set; } = ValidState.Empty;
+      public ValidState IsConfirmPasswordValid { get; set; } = ValidState.Empty;
+
+      public bool IsCompleteAndValid => this.IsFullNameValid == ValidState.Valid &&
+                                        this.IsEmailValid == ValidState.Valid &&
+                                        this.IsPasswordValid == ValidState.Valid &&
+                                        this.IsConfirmPasswordValid == ValidState.Valid;
+
+      public FormValidState(Form form)
+      {
+        this.IsFullNameValid = FormValidationUtils.GetBasicValidationState(form.FullName);
+        this.IsEmailValid = FormValidationUtils.GetBasicValidationState(form.Email);
+        this.IsPasswordValid = FormValidationUtils.GetBasicValidationState(form.Password);
+        this.IsConfirmPasswordValid = FormValidationUtils.GetBasicValidationState(form.ConfirmPassword);
+
+        if (form.Password != null &&
+            form.ConfirmPassword != null &&
+            form.ConfirmPassword != form.Password)
+        {
+          this.IsConfirmPasswordValid = ValidState.Invalid;
+        }
+      }
+    }
+
+    public Form FormValues { get; } = new Form();
+    
+    public FormValidState FormValidStates => new FormValidState(this.FormValues);
+
+    public bool CanSubmit => this.FormValidStates.IsCompleteAndValid;
 
     public bool IsPasswordPlainText { get; private set; }
     public bool IsConfirmPasswordPlainText { get; private set; }
@@ -30,7 +63,10 @@ namespace BudgetSquirrel.Client.Registration
 
     public void OnRegisterClicked()
     {
-      Console.WriteLine("Register!");
+      Console.WriteLine($"FirstName: {this.FormValues.FullName}");
+      Console.WriteLine($"Email: {this.FormValues.Email}");
+      Console.WriteLine($"Password: {this.FormValues.Password}");
+      Console.WriteLine($"ConfirmPassword: {this.FormValues.ConfirmPassword}");
     }
   }
 }
