fdc950d8dfa6577f3efabc2e05ada2a5a1474586
Ian Kirkpatrick <ikirkpat@oaisd.org>
2021-02-15 19:31:46 -0500

    Start styling register page

--- src/BudgetSquirrel.Client/Registration/RegisterPage.razor.cs
+++ src/BudgetSquirrel.Client/Registration/RegisterPage.razor.cs
@@ -4,6 +4,14 @@ namespace BudgetSquirrel.Client.Registration
 {
   public partial class RegisterPage : ComponentBase
   {
-    
+    public class Form
+    {
+      public string FullName { get; set; }
+      public string Email { get; set; }
+      public string Password { get; set; }
+      public string ConfirmPassword { get; set; }
+    }
+
+    public Form FormValues = new Form();
   }
 }
