f5c955903972125f6ab031ab49102b4a8a19709b
Ian Kirkpatrick <ianmann56@gmail.com>
2021-04-24 13:24:29 -0400

    blah

--- scripts/package_backend.sh
+++ scripts/package_backend.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-ZIP_DEST=$1
+REL_DIR="release"
+
+ZIP_DEST="$REL_DIR/Backend.zip"
 
 OUT_DIR="bin/Release/BudgetSquirrel.Backend"
 
 dotnet publish --output $OUT_DIR --configuration "Release" --framework "net5.0" /p:GenerateRuntimeConfigurationFiles=true --runtime linux-arm --self-contained false src/BudgetSquirrel.Backend/BudgetSquirrel.Backend.csproj
+
+[[ -d $REL_DIR ]] || mkdir $REL_DIR
 cd $OUT_DIR
-zip -r $ZIP_DEST *
+zip -r ../../../$ZIP_DEST *
