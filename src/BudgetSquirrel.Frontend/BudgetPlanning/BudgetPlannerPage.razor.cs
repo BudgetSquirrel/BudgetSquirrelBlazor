9581bdccb6ad1cf5e166037498a7fc69105166ba
Ian Kirkpatrick <ianmann56@gmail.com>
2021-06-13 19:42:11 -0400

    Show amount left to budget at top of budget planning page

--- src/BudgetSquirrel.Frontend/BudgetPlanning/BudgetPlannerPage.razor.cs
+++ src/BudgetSquirrel.Frontend/BudgetPlanning/BudgetPlannerPage.razor.cs
@@ -3,20 +3,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using BudgetSquirrel.Frontend.Authentication.Login;
 using Microsoft.AspNetCore.Components;
+using static BudgetSquirrel.Frontend.BudgetPlanning.BudgetPlanningContext;
 
 namespace BudgetSquirrel.Frontend.BudgetPlanning
 {
   public partial class BudgetPlannerPage : ComponentBase
   {
+    #region component arguments
+
     [Inject]
     private ILoginService loginService { get; set; }
 
     [Inject]
     private IBudgetPlanningService budgetPlanningService { get; set; }
 
+    #endregion
+
+    #region component state
+
     private BudgetPlanningContext context;
 
     private bool isLoading = true;
+
+    #endregion
+
+    #region internal methods
 
     protected override Task OnInitializedAsync()
     {
@@ -31,7 +42,11 @@ namespace BudgetSquirrel.Frontend.BudgetPlanning
       this.isLoading = false;
     }
 
-    private BudgetPlanningContext.FundBudget rootBudget => this.context.Budgets.Single(b => b.FundId == this.context.FundTree.Fund.Id);
+    #endregion
+
+    #region convenience accessors
+
+    private Budget rootBudget => this.context.FundTree.Budget;
 
     private string timeboxDisplay
     {
@@ -45,9 +60,66 @@ namespace BudgetSquirrel.Frontend.BudgetPlanning
       }
     }
 
-    private decimal PlannedIncome => this.rootBudget.Budget.PlannedAmount;
+    private decimal amountLeftToBudget
+    {
+      get
+      {
+        decimal amountBudgeted = this.context.FundTree.SubFunds.Sum(fundRelationships => fundRelationships.Budget.PlannedAmount);
+        return this.PlannedIncome - amountBudgeted;
+      }
+    }
+
+    #endregion
+
+    #region template accessors
 
     private string RootFundName => this.context.FundTree.Fund.Name;
+
+    private decimal PlannedIncome => this.rootBudget.PlannedAmount;
+
+    public bool ShouldShowAmountLeftToBudget => this.amountLeftToBudget != 0;
+
+    public string AmountLeftToBudgetLabel
+    {
+      get
+      {
+        if (this.amountLeftToBudget > 0)
+        {
+          return "Left to Budget";
+        }
+        else if (this.amountLeftToBudget < 0)
+        {
+          return "Over Budget";
+        }
+        else
+        {
+          return "";
+        }
+      }
+    }
+    
+    private string AmountLeftToBudgetDisplay => Math.Abs(this.amountLeftToBudget).ToString("C");
+
+    private string PlannedIncomeCssClass
+    {
+      get
+      {
+        string cssClass = "stat__value ";
+        if (this.amountLeftToBudget == 0)
+        {
+          cssClass += "stat__value--good";
+        }
+        else
+        {
+          cssClass += "stat__value--bad";
+        }
+        return cssClass;
+      }
+    }
+
+    #endregion
+
+    #region event handlers
 
     private async Task ChangePlannedIncome(string amount)
     {
@@ -61,5 +133,7 @@ namespace BudgetSquirrel.Frontend.BudgetPlanning
       await this.budgetPlanningService.EditFundName(this.context.FundTree.Fund.Id, newName);
       await this.ReloadContext();
     }
+
+    #endregion
   }
 }
