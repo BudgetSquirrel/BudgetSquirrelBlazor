2e3d3edaf857e434d40d08d855a9b22a1da73607
Ian Kirkpatrick <ianmann56@gmail.com>
2023-08-20 16:41:03 -0400

    Quick fix

--- src/BudgetSquirrel.Frontend/BudgetTracking/BudgetTrackingPage/Transactions/AddTransactionForm.razor.cs
+++ src/BudgetSquirrel.Frontend/BudgetTracking/BudgetTrackingPage/Transactions/AddTransactionForm.razor.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Components;
 
 namespace BudgetSquirrel.Frontend.BudgetTracking.BudgetTrackingPage.Transactions
 {
-  public class AddTransactionForm : ComponentBase
+  public partial class AddTransactionForm : ComponentBase
   {
     private string Description { get; set; } = string.Empty;
 
